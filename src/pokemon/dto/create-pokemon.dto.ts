f6f15e34138dec97d05cb1cc9b7b3fe46dbf1f7e
Jorge Bauza <jebauza1989@gmail.com>
2025-08-17 14:04:22 -0500

    Pokemon  Crud

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,15 @@
-export class CreatePokemonDto {}
+import { IsNumber, IsPositive, IsString, MaxLength, Min, MinLength } from 'class-validator';
+
+export class CreatePokemonDto {
+
+    @IsNumber()
+    @IsPositive()
+    @Min(1)
+    no: number;
+
+    @IsString()
+    @MinLength(3)
+    @MaxLength(255)
+    name: string;
+
+}
